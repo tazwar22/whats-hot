9b3cdc260540aa2f6e010cb051f946e0b7f8b314
Ahnaf Tazwar Ayub <tazwarahnaf22@gmail.com>
2021-01-02 15:38:00 -0800

    Setting up Optional Rendering

--- src/CuisineList.js
+++ src/CuisineList.js
@@ -3,26 +3,42 @@ import React, {useState} from 'react'
 
 const CuisineList = () => {
     const cuisines = [
-        {id:1, name:"Trending This Week"},
-        {id:2, name:"Mealshare"},
-        {id:3, name:"Weekend Brunches"},
-        {id:4, name:"Tasty Tacos"},
+        {id:123, name:"Trending This Week"},
+        {id:277, name:"Mealshare"},
+        {id:367, name:"Weekend Brunches"},
+        {id:444, name:"Tasty Tacos"},
         {id:5, name:"Heated patio"},
         {id:6, name:"Perfect Poutine"},
         {id:7, name:"Hidden Gems"},
     ];
     const [data, setData] = useState(cuisines);
 
+    //Flag to say whether or not a Cusine has been selected
+    const [isSelected, setIsSelected] = useState(false);
+    //Cusine ID to query
+    const [cusID, setCusID] = useState(0);
+
+    const handleClick = (id) =>{
+        setCusID(id);
+        setIsSelected(true);
+    }
+
     return (
         <>
-            <ul>
+            {!isSelected ? (
+                <ul>
                 {data.map((cuisine)=>{
                     const {id, name} = cuisine;
                     return <li key={id}>
-                                <a href="#">{name}</a>
+                                <a href="#" onClick={()=>handleClick(id)}>{name}</a>
                             </li>
                 })}
-            </ul>   
+                </ul>  
+            )
+            :(
+                <h1>Getting Restaurants... for {cusID}</h1>
+            )}
+             
         </>
     );
 }
