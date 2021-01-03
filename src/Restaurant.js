7f45d5d946d89583f41a1fde749fca06b1aac53a
Ahnaf Tazwar Ayub <tazwarahnaf22@gmail.com>
2021-01-03 13:13:33 -0800

    Added optional review display toggling

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,22 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Restaurant = (props) => {
-    const {name, positivity, star_rating, reviews} = props.restaurant
-    return (
+    const {name, positivity, reviews} = props.restaurant
+    const [showReviews, setShowReviews] = useState(false)
+
+    const clickHandler = () =>{
+        setShowReviews(!showReviews);
+    }
+
+    if (showReviews){
+            return (
+            <>
+                <ul>
+                    <li>Name: {name}</li>
+                    <li>Positivity Score: {positivity}</li>
+                    <button type="button" onClick={()=>clickHandler()}> Hide User Reviews</button>
+                    <ul>
+                        {reviews.map((rev, ii)=>{
+                        return <li key={ii}>{rev}</li>
+                        })}
+                    </ul>
+                </ul>
+                <hr></hr>
+            </>
+        );
+    } else {
+         return (
         <>
             <ul>
                 <li>Name: {name}</li>
                 <li>Positivity Score: {positivity}</li>
-                <li>Star Rating: {star_rating}</li>
-                <ul>
-                    {reviews.map((rev, ii)=>{
-                    return <li key={ii}>{rev}</li>
-                    })}
-                </ul>
+                <button type="button" onClick={()=>clickHandler()}> Show User Reviews</button>
             </ul>
             <hr></hr>
         </>
-    );
+        );
+    }
+
+   
 }
 
 export default Restaurant
