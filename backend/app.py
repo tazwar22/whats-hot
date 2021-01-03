2d6d309ee674f6b2c36f46e8d27ee16bdbcba7bf
Ahnaf Tazwar Ayub <tazwarahnaf22@gmail.com>
2021-01-02 16:34:21 -0800

    Connecting to Flask backend; getting simple data

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -7,3 +7,16 @@ CORS(app)
 @app.route('/hello')
 def say_hello_world():
     return {'result': "Hello World"}
+
+@app.route('/api/cuisines')
+def get_cusines():
+
+    data = jsonify([{"id":123, "name":"Trending This Week"},
+                    {"id":277, "name":"Mealshare"},
+                    {"id":367, "name":"Weekend Brunches"},
+                    {"id":444, "name":"Tasty Tacos"},
+                    {"id":5, "name":"Heated patio"},
+                    {"id":6, "name":"Perfect Poutine"},
+                    {"id":7, "name":"Hidden Gems"}])
+
+    return data
--- src/CuisineList.js
+++ src/CuisineList.js
@@ -1,17 +1,18 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import RestaurantList from './RestaurantList'
 
 const CuisineList = () => {
-    const cuisines = [
-        {id:123, name:"Trending This Week"},
-        {id:277, name:"Mealshare"},
-        {id:367, name:"Weekend Brunches"},
-        {id:444, name:"Tasty Tacos"},
-        {id:5, name:"Heated patio"},
-        {id:6, name:"Perfect Poutine"},
-        {id:7, name:"Hidden Gems"},
-    ];
-    const [data, setData] = useState(cuisines);
+    const [data, setData] = useState([]);
+
+    const getCusines = async () =>{
+        const response = await fetch('/api/cuisines');
+        const cuisines = await response.json();
+        setData(cuisines);
+    }
+    useEffect(()=>{
+        getCusines();
+    }, [])
+    
 
     //Flag to say whether or not a Cusine has been selected
     const [isSelected, setIsSelected] = useState(false);
